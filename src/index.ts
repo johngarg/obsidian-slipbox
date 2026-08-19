259ed782439497bebf78cbd8d1794e91aebd2ce1
John Gargalionis <johngargalionis@gmail.com>
2026-08-20 00:29:43 +1000

    feat: implement canonical zettelkasten addresses

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-// Public exports will be added as domain modules are implemented.
-export {};
+export {
+  ZettelIdError,
+  compareZettelIds,
+  formatZettelId,
+  generateFiledId,
+  generateNextSectionId,
+  incrementAlphaToken,
+  isValidZettelId,
+  parseZettelId,
+} from "./zettel-id.js";
+
+export type {
+  AlphaToken,
+  NumericToken,
+  ParsedZettelId,
+  PathToken,
+} from "./zettel-id.js";
